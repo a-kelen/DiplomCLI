1e60bbe5f89bf8d7b497eeedb5353899e644c7f9
a-kelen <39244363+a-kelen@users.noreply.github.com>
2021-05-14 19:40:57 +0300

    Add element install

--- src/Commands/installCommand.js
+++ src/Commands/installCommand.js
@@ -1,6 +1,10 @@
 const inquirer = require('inquirer')
 const axios = require('../axios')
 const { setValue , getValue, token } = require('../db')
+const fs = require('fs')
+const pathLib = require('path')
+const { type } = require('os')
+var AdmZip = require('adm-zip')
 
 function getFiles(author, name, type) {
     return new Promise((resolve, reject) => {
@@ -19,6 +23,51 @@ function getFiles(author, name, type) {
         })
     })
 }
+
+function notExistFiles(files, path = './') {    
+    for(let file of files)  {
+        if(fs.existsSync(pathLib.join(process.cwd() ,path, file)))
+            return false
+    }
+
+    return files.length > 0
+}
+
+function verifyFSAndInstall(author, elemName, elem, path) {
+    console.log('--', path)
+    return new Promise((resolve, reject) => {
+        getFiles(author, elemName, elem)
+        .then((res) => {
+            if(notExistFiles(res.files, path)) {
+                downloadFiles(res.id, elem, path)
+            }
+        })
+        .catch(() => {
+            reject()
+        })
+    })
+    
+}
+
+function downloadFiles(id, elem, path = '.') {
+    return new Promise((resolve, reject) => {
+        axios.get(`${elem}/download/${id}`, {
+            responseType: 'arraybuffer'
+        })
+        .then(res => {
+            var zip = new AdmZip(res.data);
+            // var zipEntries = zip.getEntries();
+
+            
+            // for (var i = 0; i < zipEntries.length; i++) {
+            //     console.log(zip.readAsText(zipEntries[i]));
+            // }
+            zip.extractAllTo(path, true);
+        })
+    })
+}
+
+
 
 module.exports = function (name, path, options) {
     if(options.library && options.component) {
@@ -47,11 +96,7 @@ module.exports = function (name, path, options) {
                     if(answer.confirm) {
                         let author = active.split('/')[0].slice(1)
                         let elemName = active.split('/')[1]
-                        getFiles(author, elemName, elem)
-                            .then(console.log)
-                            .catch(() => {
-                                console.log('Error')
-                            })
+                        verifyFSAndInstall(author, elemName, elem, path)
                     }
                 });
         } else if(name) {
@@ -60,20 +105,12 @@ module.exports = function (name, path, options) {
             if(vals.length === 1) {
                 elemName = name
                 getValue('username').then(val => {
-                    getFiles(val, elemName, elem)
-                        .then(console.log)
-                        .catch(() => {
-                            console.log('Error')
-                        })
+                    verifyFSAndInstall(val, elemName, elem, path)
                 })
             } else if(vals.length === 2) {
                 author = vals[0].slice(1)
                 elemName = vals[1]
-                getFiles(author, elemName, elem)
-                .then(console.log)
-                .catch(() => {
-                    console.log(vals)
-                })
+                verifyFSAndInstall(author, elemName, elem, path)
             } else {
                 console.log('Incorect format of element name')
             }
